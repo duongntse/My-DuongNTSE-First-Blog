809a0bd51d0be0013026e98c6f718f49e42f99fe
Duong Nguyen <duongntse@gmail.com>
2020-04-08 17:31:35 +0700

    updated bio.js and gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
+      facebook: `duongntse`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -59,8 +59,8 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+        <a href={`https://facebook.com/${social.facebook}`}>
+          You should follow him on Facebook
         </a>
       </p>
     </div>
